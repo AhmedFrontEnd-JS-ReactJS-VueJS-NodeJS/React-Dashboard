4e6420b9d1ca40e9c9ab6cbf7016a92eafbd283c
Ahmed Web Engineer <ahmed.vibrant1@gmail.com>
2023-02-21 23:08:22 +0500

    Revenue Section 02:17
    Revenue Section 02:17

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -38,53 +38,70 @@ const Ecommerce = () => {
           ))}
         </div>
       </div>
-      <div className='flex gap-10 flex-wrap justify-center'>
-            <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md;w-780'>
-            <div className='flex justify-between'>
-              <p className='font-semibold text-xl '>Revenue Update</p>
-              <div className='flex items-center gap-4'>
-                <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'><span><GoPrimitiveDot/></span>
-                <span>Expense</span>
-                </p>
-                <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'><span><GoPrimitiveDot/></span>
-                <span>Budget</span>
-                </p>
-
+      <div className='flex gap-10 flex-wrap'>
+              <div className='bg-white dark:text-gray-200 dark:bg-socondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+                <div className='flex justify-between '>
+                    <p className='font-semibold text-xl'> Revenue Updates</p>
+                    <div className='flex items-center gap-4 '>
+                       <p className='flex items center gap-2 text-black-400 hover:drop-shadow-xl'>
+                        <span><GoPrimitiveDot/></span>
+                        <span>Expense</span>
+                       </p>
+                       <p className='flex items center gap-2 text-green-400 hover:drop-shadow-xl'>
+                        <span><GoPrimitiveDot/></span>
+                        <span>Budget</span>
+                       </p>
+                    </div>
+                </div>
+                <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+                  <div className='border-r-1 border-color m-4 pr-10 '>
+                    <div>
+                      <p>
+                        <span className='text-3xl font-semibold'>$93000</span>
+                        <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full bg-green-400 ml-3 text-xs '>20%</span>
+                      </p>
+                      <p className='text-gray-400 mt-1'>
+                        Budget
+                      </p>
+                    </div>
+                    <div className='mt-8'>
+                      <p>
+                        <span className='text-3xl font-semibold'>$48000</span>
+                        
+                      </p>
+                      <p className='text-gray-400 mt-1'>
+                        Expense
+                      </p>
+                    </div>
+                    <div className='mt-5'>
+                  <SparkLine
+                  currentColor="blue"
+                  id="Line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                  
+                  />
+                    </div>
+                    <div className='mt-10'>
+                      <Button
+                      color="white"
+                      bgColor="blue"
+                      borderRadius="10px"
+                      text="Download Report"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <Stacked
+                    width="320px"
+                    height="360px"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className='mt-10 flex gap-10 flex-wrap justify-center'>
-              <div className='border-r-1 border-color m-4 pr-10'>
-                <div className=''>
-                  <p>
-                    <span className='text-3xl font-semi-bold '>93,000 $</span>
-                    <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
-                  </p>
-                  <p className='text-gray-500 mt-1'>
-                    Budget
-                  </p>
-                </div>
-                <div className='mt-8'>
-                  <p>
-                    <span className='text-3xl font-semi-bold '>48,000 $</span>
-                   
-                  </p>
-                  <p className='text-gray-500 mt-1'>
-                    Expense
-                  </p>
-                </div>
-                <div className='mt-5'>
-                <SparkLine
-                currentColor="blue"
-                id="line-sparkline"
-                type="Line"
-                height="80px"
-                width="250px"
-                data={SparklineAreaData}
-                />
-                </div>
-              </div>
-            </div>
-            </div>
       </div>
     </div>
   )
