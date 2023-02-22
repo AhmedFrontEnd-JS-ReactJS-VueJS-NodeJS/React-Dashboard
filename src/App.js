00e85af7f64a35b9ae128ce7a7596c5fcd4a4455
AhmedFrontEnd-JS-ReactJS-VueJS-NodeJS <ahmed.vibrant1@gmail.com>
2023-02-22 19:46:30 +0500

    2/22/2023
    2/22/2023

--- src/App.js
+++ src/App.js
@@ -38,9 +38,9 @@ function App() {
           <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
             <Navbar/>
           </div>
-       
+
         <div>
-        
+
             <Routes>
               {/* Dashboard */}
               <Route path='/' element={<Ecommerce/>}/>
